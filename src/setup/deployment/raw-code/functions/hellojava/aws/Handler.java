19fb5463c5cfbd104d463477776ed41211e31286
kk-min <76023265+kk-min@users.noreply.github.com>
2023-06-14 07:11:55 +0800

    Add Handler.java

---
+++ src/setup/deployment/raw-code/functions/hellojava/aws/Handler.java
@@ -0,0 +1,28 @@
+package hellojava;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.util.Map;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>{
+
+  @Override
+  public Void handleRequest(APIGatewayProxyRequestEvent event, Context context)
+  {
+    LambdaLogger logger = context.getLogger();
+    logger.log("EVENT TYPE: " + event.getClass());
+	int incrementLimit = event.getQueryStringParameters().getOrDefault("incrementLimit", 0);
+	this.simulateWork(incrementLimit);
+	String requestId = "no-context";
+	if (context != null) {
+		requestId = context.getAwsRequestId();	
+	}
+	APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+	response.setIsBase64Encoded(false);
+	response.setStatusCode(200);
+
+  }
+}
